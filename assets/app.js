dc7cf136e4ddea974bada46e3b973e4d3a87dd09
Tien Ping <tplim@hermo.my>
2017-07-18 01:07:14 +0800

    make home page responsive, fix menu and footer and responsive menu

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,27 @@
-var myduitApp = angular.module('myduitApp', ['ui.router']);
+(function() {
+    'use strict';
+    var myduitApp = angular.module('myduitApp', ['ui.router']);
 
-myduitApp.config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/home');
+    myduitApp.config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/home');
 
-    $stateProvider
-        .state('home', {
-            url: '/home',
-            templateUrl: 'pages/home/home-en.html'
-        })
-        .state('loantypes', {
-            url: '/loantypes',
-            templateUrl: 'pages/loantypes/loantypes-en.html'     
-        })
-        .state('apply', {
-            url: '/apply',
-            templateUrl: 'pages/apply/apply-en.html'     
-        });
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: 'pages/home/home-en.html'
+            })
+            .state('loantypes', {
+                url: '/loantypes',
+                templateUrl: 'pages/loantypes/loantypes-en.html'     
+            })
+            .state('apply', {
+                url: '/apply',
+                templateUrl: 'pages/apply/apply-en.html'     
+            });
 
-});
+    }).controller('myduitCtrl', myduitCtrl);
+
+    function myduitCtrl() {
+        var myduit = this;
+    }
+})();
