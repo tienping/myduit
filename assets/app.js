f503b8ed3e7c463ad3d3e524d255779af259592a
Tien Ping <tplim@hermo.my>
2017-08-03 00:57:39 +0800

    set language

--- assets/app.js
+++ assets/app.js
@@ -2,32 +2,52 @@
     'use strict';
     var myduitApp = angular.module('myduitApp', ['ui.router']);
 
-    myduitApp.config(function($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise('/home');
+    myduitApp
+        .config(myduitConfig)
+        .controller('myduitCtrl', myduitCtrl);
+
+    myduitConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function myduitConfig($stateProvider, $urlRouterProvider) {
+        var lang = getCookie('tp-lang') || 'en';
+
+        $urlRouterProvider.otherwise('/home_' + lang);
 
         $stateProvider.state('home', {
             url: '/home',
-            templateUrl: 'pages/home/home-en.html'
+            templateUrl: 'pages/home/home-' + lang + '.html'
         })
         .state('loantypes', {
             url: '/loantypes',
-            templateUrl: 'pages/loantypes/loantypes-en.html'     
+            templateUrl: 'pages/loantypes/loantypes-' + lang + '.html'     
         })
         .state('apply', {
             url: '/apply',
-            templateUrl: 'pages/apply/apply-en.html'     
+            templateUrl: 'pages/apply/apply-' + lang + '.html'     
         });
 
-    }).controller('myduitCtrl', myduitCtrl);
+    }
 
     myduitCtrl.$inject = [];
-
     function myduitCtrl() {
         var myduit = this;
         
-        myduit.init      = init;
-        myduit.openModal = openModal;
-        
+        myduit.init           = init;
+        myduit.openModal      = openModal;
+        myduit.toggleLanguage = toggleLanguage;
+        myduit.currentLanguage = getCookie('tp-lang') || 'en';
+
+        // myduit.data      = {
+        //     en: {},
+        //     bm: {}
+        // }
+        // loadJSON('assets/fixture-en.json', function(response) {
+        //     myduit.data.en = JSON.parse(response);
+        // });
+        // loadJSON('assets/fixture-bm.json', function(response) {
+        //     myduit.data.bm = JSON.parse(response);
+        // });
+        // myduit.lang      = myduit.lang || 'en';
+
         function init() {
             var offsetValue = 50;
             initializeSmoothScroll(offsetValue);
@@ -35,6 +55,11 @@
 
         function openModal(id) {
             $('#' + id).modal('show');
+        }
+
+        function toggleLanguage(lang) {
+            setCookie("tp-lang", lang, 3);
+            location.reload();
         }
     }
 })();
